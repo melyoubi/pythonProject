0c0a642d12dac9b21a0b4531662a15ce277f603f
elyoubi <mohamed.elyoubi.7e5@itb.cat>
2021-11-24 18:50:06 +0100

    new commit

---
+++ paythonpc/__init__.py
@@ -0,0 +1,6 @@
+mesos = ['Gener', 'Febrer', 'Març', 'Abril', 'Maig', 'Juny', 'Juliol', 'Agost', 'Septembre', 'Octubre', 'Novembre', 'Desembre']
+dia = 25
+mes = int(input())
+any = 2020
+data = str(dia) + ' de ' + mesos[mes-1] + ' del ' + str(any)
+print(data)
