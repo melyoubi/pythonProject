6a6b6f44e4a63f1732c9163eb5c5dbd90eab302f
elyoubi <mohamed.elyoubi.7e5@itb.cat>
2021-11-26 19:30:25 +0100

    new commit

--- paythonpc/itacademy/BcnActiva/ejercicoiPorter.py
+++ paythonpc/itacademy/BcnActiva/ejercicoiPorter.py
@@ -1,9 +1,10 @@
 print("que edada tienes")
 edad= int(input())
-dia=input()
-if edad >=18 :
+if edad>=18:
     print("puedes pasar")
-elif edad==0 :
+
+elif  edad==0 :
     print("introduce un edad valida")
 else :
     print("no puedes pasar")
+"""comentario"""
---
+++ paythonpc/onlinecurs/sololearn/first.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+sum = 1
+if a > b:
+    c = a
+    a = b
+    b = c
+    while a < b:
+        a = a + 1
+        sum = sum + a
+else:
+    while a < b:
+        a = a + 1
+        sum = sum + a
+print(sum)
