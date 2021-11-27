2fe75655df65d20b6f76f9cc46486bf5af4444f9
elyoubi <mohamed.elyoubi.7e5@itb.cat>
2021-11-28 00:59:16 +0100

    listfuncion

---
+++ paythonpc/onlinecourse/sololearn/ListUsefulFunctions.py
@@ -0,0 +1,15 @@
+
+'''The index method finds the first occurrence of a list item and returns its index.'''
+
+letters = ['p', 'q', 'r', 's', 'p', 'u']
+print(letters.index('r'))
+print(letters.index('p'))
+'''_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+There are a few more useful functions and methods for lists. 
+max(list): Returns the list item with the maximum value
+min(list): Returns the list item with minimum value
+list.count(item): Returns a count of how many times an item occurs in a list
+list.remove(item): Removes an object from a list
+list.reverse(): Reverses items in a list.
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+'''
