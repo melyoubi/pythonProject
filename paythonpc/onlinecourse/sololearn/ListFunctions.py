b8866643ed5cd4956af159b3e1ea54e51822b905
elyoubi <mohamed.elyoubi.7e5@itb.cat>
2021-11-28 00:50:22 +0100

    listfuncion

--- paythonpc/onlinecourse/sololearn/ListFunctions.py
+++ paythonpc/onlinecourse/sololearn/ListFunctions.py
@@ -3,3 +3,7 @@ list=[7,8,0,9]
 print(list)
 list.append(5)
 print(list)
+'''------------------------------------------------------------------------'''
+'''To get the number of items in a list, you can use the len function.'''
+listt=[7,8,0,9,9,8,7,8,9]
+print(len(listt))
