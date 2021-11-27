e3421cf726e720b8180b1ad1b95037e7f263343e
elyoubi <mohamed.elyoubi.7e5@itb.cat>
2021-11-28 00:15:32 +0100

    new commit

---
+++ paythonpc/onlinecourse/sololearn/ListOperations.py
@@ -0,0 +1,7 @@
+'''The item at a certain index in a list can be reassigned.'''
+
+nums = [7, 7, 7, 7, 7]
+nums[2] = 5
+print(nums)
+
+'''You can replace the item with an item of a different type.'''
---
+++ paythonpc/onlinecourse/sololearn/StringsLikeLists.py
@@ -0,0 +1,8 @@
+'''Some types, such as strings, can be indexed like lists.
+Indexing strings behaves as though you are indexing a list containing each character in the string.
+'''
+
+name='Mohamed Elyoubi'
+print(name[10])
+
+'''Space (" ") is also a symbol and has an index.'''
