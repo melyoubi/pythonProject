fef13b1262cbfaad706429aaf0a04506557140e5
elyoubi <mohamed.elyoubi.7e5@itb.cat>
2021-11-28 00:59:12 +0100

    listfuncion

--- paythonpc/onlinecourse/sololearn/ListFunctions.py
+++ paythonpc/onlinecourse/sololearn/ListFunctions.py
@@ -7,3 +7,11 @@ print(list)
 '''To get the number of items in a list, you can use the len function.'''
 listt=[7,8,0,9,9,8,7,8,9]
 print(len(listt))
+'''--------------------------------------------------------------------------'''
+'''The insert method is similar to append, except that it allows
+ you to insert a new item at any position in the list, 
+ as opposed to just at the end.'''
+words = ["Python", "fun"]
+index = 1
+words.insert(index, "is")
+print(words)
