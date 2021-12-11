94b9657a57ca33f521e1351a1ee39c60867c9943
elyoubi <mohamed.elyoubi.7e5@itb.cat>
2021-12-11 11:57:53 +0100

    new commit

---
+++ paythonpc/ejercicios/multivariables.py
@@ -0,0 +1,5 @@
+x = input()
+y = int(input())
+while y!=0 :
+    print(x)
+    y=y-1
---
+++ paythonpc/onlinecourse/sololearn/ReturningfromFunctions.py
@@ -0,0 +1,6 @@
+def max(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+print(max(int(input()),int(input())))
